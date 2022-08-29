0bc3e2314288b4dcc7668b892ab12178bbbbe31c
mfranco17 <88902254+mfranco17@users.noreply.github.com>
2022-08-29 05:25:07 +0200

    Cambios

---
+++ script.js
@@ -0,0 +1,14 @@
+const BoxImagen=document.getElementById("fullImgBox");
+fullimg=document.getElementById("full");
+
+
+function abrirImg(reference) {
+    BoxImagen.style.display="flex";
+    full.src=reference;
+}
+
+
+function cerrarImg() {
+    BoxImagen.style.display="none";
+}
+
